479175cd2bd54fa580ab40295a118a96ff7494ac
Geoff Elliott <tinywv@gmail.com>
2019-10-31 02:14:42 +0000

    right code, no result so I guess not right code

--- lib/cli.rb
+++ lib/cli.rb
@@ -13,13 +13,25 @@ def run
   html = open("https://en.wikipedia.org/wiki/Chelsea_F.C.")
   doc = Nokogiri::HTML(html)
   facts = []
-  doc.css("tbody tr")[1].css("th").each do |fact|
-    fullname = fact.css("td").text
+  doc.css("tbody tr")[1].each do |fact|
+    fullname = fact.css("td")[0]
     facts << fullname
   end
 
   facts.each.with_index(1) do |fullname, index|
     puts "#{index}. #{fullname}"
   end
+  
+  # html = open("https://www.imdb.com/")
+  # doc = Nokogiri::HTML(html)
+  # movies = []
+  # doc.css("div.rhs-body")[0].css("div.rhs-row").each do |mov|
+  #   title = mov.css(".title").text.strip
+  #   movies << title
+  # end
+
+  # movies.each.with_index(1) do |title, index|
+  #   puts "#{index}. #{title}"
+  # end
   
 end
