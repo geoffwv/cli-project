904323057f8e46193279fb4d318b6184bc902f89
Geoff Elliott <tinywv@gmail.com>
2019-10-31 01:39:15 +0000

    testing run process

---
+++ lib/cli.rb
@@ -0,0 +1,20 @@
+require 'nokogiri'
+require 'open-uri'
+
+def run
+  puts ""
+  puts ""
+  puts "Welcome to More Than You Ever Wanted To Know About Chelsea FC!"
+  puts "This is a repository of all things Chelsea...or at least a few of them."
+  puts " "
+  puts ": ======================= :"
+  puts " "
+
+  # html = open("https://en.wikipedia.org/wiki/Chelsea_F.C.")
+  # doc = Nokogiri::HTML(html)
+
+  # binding.pry
+  
+  end
+
+end
